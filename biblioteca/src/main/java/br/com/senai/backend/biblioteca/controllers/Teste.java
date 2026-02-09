613532e10d9529aef803badb9eaee78a59271fc7
jvzin666 <veralellinda@gmail.com>
2026-02-09 11:26:34 -0300

    sdfdfsdaf

---
+++ biblioteca/src/main/java/br/com/senai/backend/biblioteca/controllers/Teste.java
@@ -0,0 +1,16 @@
+package br.com.senai.backend.biblioteca.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class Teste {
+
+    @GetMapping()
+    public String testeUm () {
+        return "Entrei no tete um";
+    }
+
+}
